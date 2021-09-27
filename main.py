5ccc6ba891aa9442561adb0dd326bd441f5c3e76
Ness <53577182+nirpython@users.noreply.github.com>
2021-09-27 01:39:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+string="['Directors - NUMEL SOLUTIONS PRIVATE LIMITED - Toflerhttps://www.tofler.in › ... › Computer Related Serviceshttps://www.tofler.in › ... › Computer Related ServicesNumel Solutions Private Limited has three directors - Kalyan Vurukuti, Pradeep Chandra Behara, and others. The Corporate Identification Number (CIN) of Numel\xa0...',"
+new_string=""
+
+for i in string:
+    if(i.isalpha() or i==" " or i.isdigit()):
+        new_string+=i
+print(new_string)
+
+mylist=["director","companyname","official account","linkedinaccount",]
+print(mylist)
